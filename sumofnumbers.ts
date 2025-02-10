0dc59ea604b90ba4bef8ba2b9e540f43f67bbcb7
Ruhi Pirnia <147004419+rpirnia@users.noreply.github.com>
2025-02-09 18:45:14 -1000

    Added functions that return the sum of numbers

---
+++ sumofnumbers.ts
@@ -0,0 +1,34 @@
+// Takes a list of numbers and returns the sum of those numbers
+function sumFor(nums:number[]):number {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+}
+console.log(sumFor([1, 2, 3, 4])); // 10
+
+function sumWhile(nums:number[]):number {
+  let sum = 0;
+  let i = 0;
+  while (i < nums.length) {
+    sum += nums[i];
+    i++;
+  }
+  return sum;
+}
+console.log(sumWhile([1, 2, 3, 4])); // 10
+
+function sumRecursion(nums:number[]):number {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1));
+}
+console.log(sumRecursion([1, 2, 3, 4])); // 10
+
+function sumTheFunctionalWay(nums:number[]):number {
+  const sum = nums.reduce((acc, val) => acc + val, 0);
+  return sum;
+}
+console.log(sumTheFunctionalWay([1, 2, 3, 4])); // 10
